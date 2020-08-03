1ea33dc6cd5b6ddf346ac6fbba28b300b5ec7f14
Fortie40 <elijahmurimi40@gmail.com>
2020-08-03 14:13:12 +0300

    Initial Commit

---
+++ app/src/main/java/com/fortie40/movieapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.fortie40.movieapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
