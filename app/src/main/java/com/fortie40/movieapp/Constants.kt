b8acf569f32e481a789f1af188391e4aa41a1b0c
Fortie40 <elijahmurimi40@gmail.com>
2020-08-04 13:26:27 +0300

    Add Splash Screen

--- app/src/main/java/com/fortie40/movieapp/Constants.kt
+++ app/src/main/java/com/fortie40/movieapp/Constants.kt
@@ -1,2 +1,4 @@
 package com.fortie40.movieapp
 
+// Api key for using the TMDb
+const val API_KEY = "45955df5a808804d70bf784d00333ec9"
--- app/src/main/java/com/fortie40/movieapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/fortie40/movieapp/ui/main/MainActivity.kt
@@ -10,6 +10,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var activityMainBinding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
