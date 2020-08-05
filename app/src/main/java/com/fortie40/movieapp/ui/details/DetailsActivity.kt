50ad042baf2b2f0ab1b2b5fe0f0343f917ba2658
Fortie40 <elijahmurimi40@gmail.com>
2020-08-05 16:56:47 +0300

    create DealsActivity layout and transform it to a class

--- app/src/main/java/com/fortie40/movieapp/ui/details/DetailsActivity.kt
+++ app/src/main/java/com/fortie40/movieapp/ui/details/DetailsActivity.kt
@@ -1,2 +1,16 @@
 package com.fortie40.movieapp.ui.details
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.fortie40.movieapp.R
+import com.fortie40.movieapp.databinding.ActivityDetailsBinding
+
+class DetailsActivity : AppCompatActivity() {
+    private lateinit var activityDetailsBinding: ActivityDetailsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activityDetailsBinding = DataBindingUtil.setContentView(this, R.layout.activity_details)
+    }
+}
