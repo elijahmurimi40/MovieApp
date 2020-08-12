5ff1a023d57d26d635ee3bdc38d24140ce794d7e
Fortie40 <elijahmurimi40@gmail.com>
2020-08-12 11:46:59 +0300

    Change theme to dark mode

--- app/src/main/java/com/fortie40/movieapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/fortie40/movieapp/ui/main/MainActivity.kt
@@ -3,6 +3,7 @@ package com.fortie40.movieapp.ui.main
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.databinding.DataBindingUtil
 import com.fortie40.movieapp.R
 import com.fortie40.movieapp.databinding.ActivityMainBinding
@@ -14,6 +15,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.AppTheme)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
         super.onCreate(savedInstanceState)
         activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
