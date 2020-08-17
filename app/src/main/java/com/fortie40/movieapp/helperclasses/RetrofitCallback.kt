98b31e6c7943e13319ac79c42673646baf7de1e1
Fortie40 <elijahmurimi40@gmail.com>
2020-08-17 16:39:33 +0300

    Refactor code in MovieDataSource

---
+++ app/src/main/java/com/fortie40/movieapp/helperclasses/RetrofitCallback.kt
@@ -0,0 +1,40 @@
+package com.fortie40.movieapp.helperclasses
+
+import androidx.lifecycle.MutableLiveData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.io.IOException
+
+object RetrofitCallback {
+    fun <T> Call<T>.enqueueCallBack(
+        networkState: MutableLiveData<NetworkState>,
+        success: (Response<T>) -> Unit,
+        endOfList: Boolean = false) {
+
+        this.enqueue(object : Callback<T> {
+            override fun onFailure(call: Call<T>, t: Throwable) {
+                when (t) {
+                    is IOException -> networkState.postValue(NetworkState.ERROR)
+                    else -> networkState.postValue(NetworkState.ERROR)
+                }
+            }
+
+            override fun onResponse(call: Call<T>, response: Response<T>) {
+                when {
+                    response.isSuccessful && response.body() != null -> {
+                        success.invoke(response)
+                    }
+                    response.body() == null -> {
+                        if (endOfList)
+                            networkState.postValue(NetworkState.END_OF_LIST)
+                        else
+                            networkState.postValue(NetworkState.ERROR)
+                    }
+                    response.code() == 401 -> networkState.postValue(NetworkState.ERROR)
+                    else -> networkState.postValue(NetworkState.ERROR)
+                }
+            }
+        })
+    }
+}
--- app/src/main/java/com/fortie40/movieapp/ui/list/MovieDataSource.kt
+++ app/src/main/java/com/fortie40/movieapp/ui/list/MovieDataSource.kt
@@ -5,14 +5,12 @@ import androidx.lifecycle.MutableLiveData
 import androidx.paging.PageKeyedDataSource
 import com.fortie40.movieapp.FIRST_PAGE
 import com.fortie40.movieapp.helperclasses.NetworkState
+import com.fortie40.movieapp.helperclasses.RetrofitCallback.enqueueCallBack
 import com.fortie40.movieapp.models.Movie
 import com.fortie40.movieapp.models.MovieResponse
 import com.fortie40.movieapp.retrofitservices.IPopularMovies
 import com.fortie40.movieapp.retrofitservices.RetrofitBuilder
-import retrofit2.Call
-import retrofit2.Callback
 import retrofit2.Response
-import java.io.IOException
 
 class MovieDataSource: PageKeyedDataSource<Int, Movie>() {
     private val page = FIRST_PAGE
@@ -25,61 +23,32 @@ class MovieDataSource: PageKeyedDataSource<Int, Movie>() {
     ) {
         _networkState.postValue(NetworkState.LOADING)
 
+        fun success(response: Response<MovieResponse>) {
+            callback.onResult(response.body()!!.movieList, null, page + 1)
+            _networkState.postValue(NetworkState.LOADED)
+        }
+
         RetrofitBuilder.buildService(IPopularMovies::class.java)
             .getPopularMovies(page)
-            .enqueue(object : Callback<MovieResponse> {
-                override fun onFailure(call: Call<MovieResponse>, t: Throwable) {
-                    when (t) {
-                        is IOException -> _networkState.postValue(NetworkState.ERROR)
-                        else -> _networkState.postValue(NetworkState.ERROR)
-                    }
-                }
-
-                override fun onResponse(call: Call<MovieResponse>, response: Response<MovieResponse>
-                ) {
-                    when {
-                        response.isSuccessful && response.body() != null -> {
-                            callback.onResult(response.body()!!.movieList, null, page + 1)
-                            _networkState.postValue(NetworkState.LOADED)
-                        }
-                        response.code() == 401 -> _networkState.postValue(NetworkState.ERROR)
-                        else -> _networkState.postValue(NetworkState.ERROR)
-                    }
-                }
-            })
+            .enqueueCallBack(_networkState, ::success)
     }
 
     override fun loadAfter(params: LoadParams<Int>, callback: LoadCallback<Int, Movie>) {
         _networkState.postValue(NetworkState.LOADING)
 
+        fun success(response: Response<MovieResponse>) {
+            val hasMore = response.body()!!.totalPages >= params.key
+            if (hasMore) {
+                callback.onResult(response.body()!!.movieList, params.key + 1)
+                _networkState.postValue(NetworkState.LOADED)
+            } else {
+                _networkState.postValue(NetworkState.END_OF_LIST)
+            }
+        }
+
         RetrofitBuilder.buildService(IPopularMovies::class.java)
             .getPopularMovies(params.key)
-            .enqueue(object : Callback<MovieResponse> {
-                override fun onFailure(call: Call<MovieResponse>, t: Throwable) {
-                    when (t) {
-                        is IOException -> _networkState.postValue(NetworkState.ERROR)
-                        else -> _networkState.postValue(NetworkState.ERROR)
-                    }
-                }
-
-                override fun onResponse(call: Call<MovieResponse>, response: Response<MovieResponse>
-                ) {
-                    when {
-                        response.isSuccessful && response.body() != null -> {
-                            val hasMore = response.body()!!.totalPages >= params.key
-                            if (hasMore) {
-                                callback.onResult(response.body()!!.movieList, params.key + 1)
-                                _networkState.postValue(NetworkState.LOADED)
-                            } else {
-                                _networkState.postValue(NetworkState.END_OF_LIST)
-                            }
-                        }
-                        response.body() == null -> _networkState.postValue(NetworkState.END_OF_LIST)
-                        response.code() == 401 -> _networkState.postValue(NetworkState.ERROR)
-                        else -> _networkState.postValue(NetworkState.ERROR)
-                    }
-                }
-            })
+            .enqueueCallBack(_networkState, ::success, true)
     }
 
     override fun loadBefore(params: LoadParams<Int>, callback: LoadCallback<Int, Movie>) {
