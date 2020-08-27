98daa4a6b06f585a2735b184415c3044e5671a6a
Fortie40 <elijahmurimi40@gmail.com>
2020-08-27 17:12:02 +0300

    Add poster path

--- app/src/main/java/com/fortie40/movieapp/bindingadapters/ActivityDetailsBindingAdapters.kt
+++ app/src/main/java/com/fortie40/movieapp/bindingadapters/ActivityDetailsBindingAdapters.kt
@@ -1,16 +1,30 @@
 package com.fortie40.movieapp.bindingadapters
 
 import android.view.View
+import android.widget.ImageView
 import android.widget.ProgressBar
 import androidx.databinding.BindingAdapter
+import com.fortie40.movieapp.data.models.MovieDetails
 import com.fortie40.movieapp.helperclasses.NetworkState
 
-@BindingAdapter("setVisibility")
-fun setVisibility(view: View, networkState: NetworkState?) {
+@BindingAdapter("setVisibilityDetails")
+fun setVisibilityDetails(view: View, networkState: NetworkState?) {
     networkState.let {
         when (view) {
             is ProgressBar -> view.visibility = if (it == NetworkState.LOADING || it == null) View.VISIBLE else View.GONE
             else -> view.visibility = if (it == NetworkState.ERROR) View.VISIBLE else View.GONE
+        }
+    }
+}
+
+@BindingAdapter("setUpDetails")
+fun setUpDetails(view: View, movieDetails: MovieDetails?) {
+    movieDetails.let {
+        when (view) {
+            is ImageView -> {
+                if (it != null)
+                    setImage(view, it.posterPath)
+            }
         }
     }
 }
--- app/src/main/java/com/fortie40/movieapp/data/models/MovieDetails.kt
+++ app/src/main/java/com/fortie40/movieapp/data/models/MovieDetails.kt
@@ -1,5 +1,10 @@
 package com.fortie40.movieapp.data.models
 
+import com.google.gson.annotations.SerializedName
+
 data class MovieDetails(
-    val title: String
+    val title: String,
+
+    @SerializedName("poster_path")
+    val posterPath: String
 )
