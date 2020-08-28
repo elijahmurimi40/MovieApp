9720c72ef22a549ca95554541d3276d920fe89a5
Fortie40 <elijahmurimi40@gmail.com>
2020-08-28 15:48:40 +0300

    Add features to activity_details

--- app/src/main/java/com/fortie40/movieapp/bindingadapters/ActivityDetailsBindingAdapters.kt
+++ app/src/main/java/com/fortie40/movieapp/bindingadapters/ActivityDetailsBindingAdapters.kt
@@ -3,7 +3,9 @@ package com.fortie40.movieapp.bindingadapters
 import android.view.View
 import android.widget.ImageView
 import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import com.fortie40.movieapp.R
 import com.fortie40.movieapp.data.models.MovieDetails
 import com.fortie40.movieapp.helperclasses.NetworkState
 
@@ -26,5 +28,18 @@ fun setUpDetails(view: View, movieDetails: MovieDetails?) {
                     setImage(view, it.posterPath)
             }
         }
+    }
+}
+
+@BindingAdapter("setTextDateRunTime")
+fun setTextDateRunTime(tv: TextView, movieDetails: MovieDetails?) {
+    if (movieDetails != null) {
+        val year = movieDetails.releaseDate.substring(0, 4)
+
+        val hour = movieDetails.runtime.div(60)
+        val minute = movieDetails.runtime.minus(hour.times(60))
+        val time = tv.context.getString(R.string.h_min, hour, minute)
+
+        tv.text = tv.context.getString(R.string.date_time, year, time)
     }
 }
--- app/src/main/java/com/fortie40/movieapp/data/models/MovieDetails.kt
+++ app/src/main/java/com/fortie40/movieapp/data/models/MovieDetails.kt
@@ -4,7 +4,10 @@ import com.google.gson.annotations.SerializedName
 
 data class MovieDetails(
     val title: String,
+    val runtime: Int,
 
     @SerializedName("poster_path")
-    val posterPath: String
+    val posterPath: String,
+    @SerializedName("release_date")
+    val releaseDate: String
 )
