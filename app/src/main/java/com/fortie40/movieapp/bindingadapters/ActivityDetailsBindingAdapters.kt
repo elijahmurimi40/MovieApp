1dcba799c2cb9f12df5b1a6ddd4cb9b02ee3476e
Fortie40 <elijahmurimi40@gmail.com>
2020-08-29 16:55:57 +0300

    add blur to image

--- app/src/main/java/com/fortie40/movieapp/bindingadapters/ActivityDetailsBindingAdapters.kt
+++ app/src/main/java/com/fortie40/movieapp/bindingadapters/ActivityDetailsBindingAdapters.kt
@@ -1,33 +1,71 @@
 package com.fortie40.movieapp.bindingadapters
 
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.view.View
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.doOnPreDraw
 import androidx.databinding.BindingAdapter
+import com.fortie40.movieapp.POSTER_BASE_URL
 import com.fortie40.movieapp.R
 import com.fortie40.movieapp.data.models.MovieDetails
 import com.fortie40.movieapp.helperclasses.NetworkState
+import com.squareup.picasso.Picasso
+import com.squareup.picasso.Target
+import jp.wasabeef.picasso.transformations.BlurTransformation
+
+private fun setImageDetails(iv: ImageView, poster_path: String?) {
+    val target = object : Target {
+        override fun onPrepareLoad(placeHolderDrawable: Drawable?) {
+            iv.scaleType = ImageView.ScaleType.CENTER
+            iv.setImageResource(R.drawable.place_holder)
+        }
+
+        override fun onBitmapFailed(e: Exception?, errorDrawable: Drawable?) {
+            iv.scaleType = ImageView.ScaleType.CENTER
+            iv.setImageResource(R.drawable.error)
+        }
+
+        override fun onBitmapLoaded(bitmap: Bitmap?, from: Picasso.LoadedFrom?) {
+            iv.doOnPreDraw {
+                if (bitmap != null) {
+                    iv.requestLayout()
+                    iv.layoutParams.height = ConstraintLayout.LayoutParams.MATCH_PARENT
+                }
+            }
+            iv.scaleType = ImageView.ScaleType.FIT_XY
+            iv.setImageBitmap(bitmap)
+        }
+    }
+    val transformation = BlurTransformation(iv.context)
+
+    iv.tag = target
+
+    Picasso.get()
+        .load(POSTER_BASE_URL + poster_path)
+        .transform(transformation)
+        .into(target)
+}
 
 @BindingAdapter("setVisibilityDetails")
 fun setVisibilityDetails(view: View, networkState: NetworkState?) {
     networkState.let {
         when (view) {
-            is ProgressBar -> view.visibility = if (it == NetworkState.LOADING || it == null) View.VISIBLE else View.GONE
+            is ProgressBar -> view.visibility =
+                if (it == NetworkState.LOADING || it == null) View.VISIBLE else View.GONE
             else -> view.visibility = if (it == NetworkState.ERROR) View.VISIBLE else View.GONE
         }
     }
 }
 
 @BindingAdapter("setUpDetails")
-fun setUpDetails(view: View, movieDetails: MovieDetails?) {
+fun setUpDetails(iv: ImageView, movieDetails: MovieDetails?) {
     movieDetails.let {
-        when (view) {
-            is ImageView -> {
-                if (it != null)
-                    setImage(view, it.posterPath)
-            }
-        }
+        if (it != null)
+            setImageDetails(iv, it.posterPath)
     }
 }
 
